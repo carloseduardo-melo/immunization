66a1fc6f08b4e517991d5f7440764c9f678ab7e8
carloseduardo-melo <87390779+carloseduardo-melo@users.noreply.github.com>
2026-08-12 18:12:53 -0300

    Fix/ci cd (#40)
    * feat: implementado configuracao inicial docker
    
    * feat: implementando schema
    
    * feat: etl implementado para alimentar o banco
    
    * feat: validacao pos carga do etl
    
    * feat: implementado tela login e rotas backend autenticaçao jwt
    
    * test: cobertuta de test
    
    * test: pre push
    
    * Remove node_modules from repository
    
    * test: pre push - husky
    
    * test: pre push - husky - tests
    
    * fix: corrigindo ci cd
    
    * fix: corrigindo ci cd
    
    * fix: run tests ci
    
    * CI: fix workflow heredoc indentation; make test DB connect_args conditional
    
    * fix: run tests ci

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -9,7 +9,14 @@ os.environ["TESTING"] = "1"
 os.environ.setdefault("DATABASE_URL", "sqlite:///./test.db")
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+if connect_args:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
